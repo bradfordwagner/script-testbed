db794a7c2cab9630f6b53791d718549f4f739378
bradfordwagner <wagner.bradford@gmail.com>
2022-12-05 15:06:42 -0500

    add color to set x

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@
 d: dev
 dev: watch
 watch:
-	@watchexec -cr -w ./test.sh -w Makefile -- make dev_pipeline
+	@watchexec -cr -w ./main.sh -w Makefile -- make dev_pipeline
 dev_pipeline: run
 
 run:
-	./test.sh
+	./main.sh
--- main.sh
+++ main.sh
@@ -1,11 +1,14 @@
 
 echo ${palette_lpurple}running script ${palette_restore}
 echo ${palette_lpurple}****************************************** ${palette_restore}
+PS4='\033[1;34m$(date +%H:%M:%S)\033[0m '
+set -ex
 ## SCRIPT #########################################################
 
-
+echo hi friends
 
 ###################################################################
+set +ex
 
 echo ${palette_lpurple}****************************************** ${palette_restore}
 echo ${palette_lpurple}finished! ${palette_restore}
